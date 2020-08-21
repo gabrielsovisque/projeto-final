c5101f43b140ff70da9fa5181722f4f5f0d6130b
Gabriel <reisgabriel14@hotmail.com>
2020-08-20 21:18:09 -0300

    configurei o banco de dados com a class sql usando PDO para depois poder usar o sistema DAO completo no projeto ;)

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitc430a2c77e9989a949c71b6dda7304d8
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -41,7 +51,8 @@ class ComposerStaticInitc430a2c77e9989a949c71b6dda7304d8
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInitc430a2c77e9989a949c71b6dda7304d8::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitc430a2c77e9989a949c71b6dda7304d8::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitc430a2c77e9989a949c71b6dda7304d8::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInitc430a2c77e9989a949c71b6dda7304d8::$prefixesPsr0;
             $loader->classMap = ComposerStaticInitc430a2c77e9989a949c71b6dda7304d8::$classMap;
 
